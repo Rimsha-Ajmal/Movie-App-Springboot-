b832e35e00900f30aa8b230de4e22017ea00393d
Rimsha-Ajmal <114993374+Rimsha-Ajmal@users.noreply.github.com>
2024-10-05 16:25:50 -0700

    init commit

---
+++ Backend/movies/src/main/java/dev/rimsha/movies/repo/MovieRepository.java
@@ -0,0 +1,14 @@
+package dev.rimsha.movies.repo;
+
+import dev.rimsha.movies.Movie;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface MovieRepository extends MongoRepository<Movie, ObjectId> {
+
+    Optional<Movie> findMovieByImdbId(String imbdId);
+}
